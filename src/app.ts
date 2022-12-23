837d0febc2ac502a7657d970086c108bc5eb8637
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-12-23 07:16:31 -0300

    feat: :sparkles: add all the routes to the application

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import express, { Express, json } from 'express';
 
 import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware';
+import router from './routes/router';
+
 dotenv.config();
 
 const app: Express = express();
@@ -11,6 +13,7 @@ const app: Express = express();
 app.use([
   cors(),
   json(),
+  router,
   errorHandlerMiddleware]);
 
 export default app;
