38bcbc8ce5cd6b74b32fe218fe4895002ad08a01
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-12-23 06:38:47 -0300

    feat: :sparkles: add a catch error function to the application

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
+import 'express-async-errors';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { Express, json } from 'express';
 
+import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware';
 dotenv.config();
 
 const app: Express = express();
 
-app.use([json(), cors()]);
+app.use([
+  cors(),
+  json(),
+  errorHandlerMiddleware]);
 
 export default app;
