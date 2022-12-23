44f1cfbcad1de664eb9e83ed41f398c417feb0d8
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-12-23 07:09:34 -0300

    feat: :sparkles: add a return statement with a formated version of external api

--- src/services/addressService.ts
+++ src/services/addressService.ts
@@ -1,15 +1,23 @@
+import { AddressAPICorrect, AddressAPIIncorrect } from '../types/addressTypes';
+import cepApi from '../utils/cepApiUtils';
+import { badRequestError, generateThrowErrorMessages } from '../utils/errorUtils';
+import { excludeProperty } from '../utils/excludePropertiesUtils';
+
 async function getAddressByCep(cep: string) {
   if (isNaN(Number(cep))) badRequestError("CEP input must contains only numbers!");
 
   if (cep.length > 8) badRequestError("CEP length must be not bigger than 8 numbers!");
 
   const apiInfo = await getAddressByAPi(cep);
+
   if (!apiInfo.ok) {
     generateThrowErrorMessages(apiInfo.statusText, (apiInfo as AddressAPIIncorrect).message);
-}
+  }
 
-async function getAddressByAPi(cep: string) {
-  return { "status": 200, "ok": true, "code": "20785-355", "state": "RJ", "city": "Rio de Janeiro", "district": "Maria da Graça", "address": "Rua Domingos de Magalhães - de 534 ao fim - lado par", "statusText": "ok" }
+  if (apiInfo.ok) {
+    return excludeProperty(apiInfo as AddressAPICorrect, "ok", "status", "statusText");
+  }
+}
 
 async function getAddressByAPi(cep: string): Promise<AddressAPICorrect | AddressAPIIncorrect> {
   const response = await cepApi.get<AddressAPICorrect | AddressAPIIncorrect>('/cep.json', {
